ce9b33104f8183d94a39a04a6fa33d82bec35d8c
whoiswentz <wentz.vinicios@gmail.com>
2024-08-30 17:20:14 -0300

    feat: add Room

---
+++ priv/repo/migrations/20240830201456_create_rooms.exs
@@ -0,0 +1,12 @@
+defmodule Slax.Repo.Migrations.CreateRooms do
+  use Ecto.Migration
+
+  def change do
+    create table(:rooms) do
+      add :name, :string, null: false
+      add :topic, :string
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
