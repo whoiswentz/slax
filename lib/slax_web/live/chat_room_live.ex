da48576f06c661ffce28079ed530555a282695db
whoiswentz <wentz.vinicios@gmail.com>
2024-09-04 20:13:49 -0300

    feat: list all rooms in sidebar

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -5,10 +5,12 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat.Room
 
   def mount(_params, _session, socket) do
-    room = Room |> Repo.all() |> List.first()
+    rooms = Room |> Repo.all()
+    room = List.first(rooms)
 
     {:ok,
      socket
+     |> assign(:rooms, rooms)
      |> assign(:room, room)
      |> assign(hide_topic?: false)}
   end
@@ -17,8 +19,45 @@ defmodule SlaxWeb.ChatRoomLive do
     {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        <%= @room.name %>
+      </span>
+    </a>
+    """
+  end
+
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Slax
+          </h1>
+        </div>
+      </div>
+      <div class="mt-4 auto">
+        <div class="flex items-center h-8 px-3 group">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+        <div id="rooms-list">
+          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
     <div class="flex flex-col flex-grow shadow-lg">
       <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
