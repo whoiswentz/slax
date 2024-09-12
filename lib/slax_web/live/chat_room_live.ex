ab7376fb34f7064aa5ea254b50aae8cfb637001b
whoiswentz <wentz.vinicios@gmail.com>
2024-09-12 10:06:25 -0300

    feat: show timestamps in correct timezone

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -8,9 +8,12 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
 
+    timezone = get_connect_params(socket)["timezone"]
+
     {:ok,
      socket
      |> assign(:rooms, rooms)
+     |> assign(timezone: timezone)
      |> assign(hide_topic?: false)}
   end
 
@@ -88,6 +91,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :message, Message, required: true
   attr :dom_id, :string, required: true
+  attr :timezone, :string, required: true
 
   defp message(assigns) do
     ~H"""
@@ -97,7 +101,9 @@ defmodule SlaxWeb.ChatRoomLive do
         <.link class="text-sm font-semibold hover:underline">
           <span><%= username(@message.user) %></span>
         </.link>
-        <span class="ml-1 text-xs text-gray-500"><%= message_timestamp(@message) %></span>
+        <span :if={@timezone} class="ml-1 text-xs text-gray-500">
+          <%= message_timestamp(@message, @timezone) %>
+        </span>
         <p class="text-sm"><%= @message.body %></p>
       </div>
     </div>
@@ -188,7 +194,12 @@ defmodule SlaxWeb.ChatRoomLive do
         </ul>
       </div>
       <div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
-        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
+        <.message
+          :for={{dom_id, message} <- @streams.messages}
+          dom_id={dom_id}
+          message={message}
+          timezone={@timezone}
+        />
       </div>
       <div class="h-12 bg-white px-4 pb-4">
         <.form
@@ -218,8 +229,9 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
     message.inserted_at
+    |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
   end
 end
