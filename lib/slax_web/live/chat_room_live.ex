77865f077def37cf91cad9bde9db5755874632a1
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-04-14 21:42:38 -0300

    remove header from layout
    Signed-off-by: Vinicios Wentz <wentz.vinicios@gmail.com>

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -4,6 +4,9 @@ defmodule SlaxWeb.ChatRoomLive do
   def render(assigns) do
     ~H"""
     <div>Welcome to the chat!</div>
+    <Layouts.app flash={@flash}>
+      <h1>Chat Room</h1>
+    </Layouts.app>
     """
   end
 end
